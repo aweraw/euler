14d8b49472a3fbffb832c702299ac801a9ef6883
Aidan Williams <aweraw@gmail.com>
2018-09-20 20:46:47 +1000

    problem 86 solution

--- e86.py
+++ e86.py
@@ -1,5 +1,13 @@
-from itertools import count, takewhile
 
-M = 100
+cnt = 0
+target = 1000000
+adj = 2
 
+while cnt < target:
+    adj += 1
+    for oppos in range(3, 2*adj + 1):
+        hyp = (adj**2 + oppos**2)**0.5
+        if hyp == int(hyp):
+            cnt += int(oppos/2.0) if oppos <= adj else (adj - int((oppos+1)/2.0)) + 1
 
+print adj
